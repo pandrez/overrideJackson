ebb492f4ba26fd97e921cacfee88a6944ce76e8c
Pedro Andrez <pedro.andrez@mercedes-benz.io>
2022-03-28 16:39:26 +0100

    Initial Commit

---
+++ src/main/kotlin/com/example/overridejackson/OverridejacksonApplication.kt
@@ -0,0 +1,11 @@
+package com.example.overridejackson
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OverridejacksonApplication
+
+fun main(args: Array<String>) {
+	runApplication<OverridejacksonApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/overridejackson/OverridejacksonApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.overridejackson
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OverridejacksonApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
